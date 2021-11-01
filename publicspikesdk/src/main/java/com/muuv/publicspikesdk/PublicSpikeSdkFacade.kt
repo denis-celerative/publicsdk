e4e1ac931b45c5491c8f03abd1b0c4cb98cbe4ab
Denis Reinoso <denis.reinoso@celerative.com>
2021-11-01 14:30:16 -0300

    Upgrade version and fixes for compilation in jitpack

--- publicspikesdk/src/main/java/com/muuv/publicspikesdk/PublicSpikeSdkFacade.kt
+++ publicspikesdk/src/main/java/com/muuv/publicspikesdk/PublicSpikeSdkFacade.kt
@@ -1,15 +1,18 @@
 package com.muuv.publicspikesdk
 
-import android.telephony.PhoneNumberUtils
-import com.muuv.spikesdk.util.SpikePhoneValidator
+//import com.muuv.spikesdk.util.SpikePhoneValidator
 
 object PublicSpikeSdkFacade {
 
-    fun validatePhone(phone: String?): Boolean {
-        return SpikePhoneValidator.validatePhone(phone)
-    }
+//    fun validatePhone(phone: String?): Boolean {
+//        return SpikePhoneValidator.validatePhone(phone)
+//    }
+//
+//    fun formatPhoneWithCountry(phone: String, countryISO: String): String? {
+//        return SpikePhoneValidator.formatPhoneWithCountry(phone, countryISO)
+//    }
 
-    fun formatPhoneWithCountry(phone: String, countryISO: String): String? {
-        return SpikePhoneValidator.formatPhoneWithCountry(phone, countryISO)
+    fun getMessage(): String {
+        return "This is the PublicSpikeSdkFacade"
     }
 }
